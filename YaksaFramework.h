026c301fe880860a1afa107497ae0a90927589dc
piyoma <249300887@qq.com>
2020-04-29 21:38:12 +0800

    ALL

--- YaksaFramework.h
+++ YaksaFramework.h
@@ -12,7 +12,7 @@
 ////                  A cross-platform GUI Framework Wrapped Chromium that Can embedded All Systems and Platforms.
 ////                  eg. Windows, linux, macOS, Android, iOS, Unity, UnrealEngine4, Qt, and etc.
 ////                  2020/04/29 piyoma
-////
+////                  break;
 ////                  ༺GitHub https://github.com/piyoma/YaksaFramework༻    
 ////                                                
 ////--꧁༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒༒꧂
