ca37d26b25f6471220c5c7c58dffb0bb1b2e5ddc
piyoma <249300887@qq.com>
2020-04-30 23:44:07 +0800

    ALL

--- YaksaFramework.h
+++ YaksaFramework.h
@@ -90,13 +90,13 @@ namespace Yaksa{
 
 //-------------------------꧁༒༒༒DEFINE TYPES༻༒༒༒-------------------------꧂
 
-				#define YAKSA_DEFINE_TYPE(NAME, obj, type_def, type, ptr_det, ptr);\
-						template <typename WrapperType, typename Type = obj>\
-						using NAME = typename TemplateType<WrapperType, Type>::_Type;\
-						template <typename WrapperType, typename Type = type>\
-						using type_def = typename TemplateType<WrapperType, Type>::_Type;\
-						template <typename WrapperType, typename Type = sub>\
-						using ptr_det = typename TemplateType<WrapperType, Type>::_Type*;
+#define YAKSA_DEFINE_TYPE(NAME, obj, type_def, type, ptr_det, ptr);\
+				template <typename WrapperType, typename Type = obj>\
+				using NAME = typename TemplateType<WrapperType, Type>::_Type;\
+				template <typename WrapperType, typename Type = type>\
+				using type_def = typename TemplateType<WrapperType, Type>::_Type;\
+				template <typename WrapperType, typename Type = sub>\
+				using ptr_det = typename TemplateType<WrapperType, Type>::_Type*;
 
 
 #endif // !YAKSA_DEFINE_TYPE
